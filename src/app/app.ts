2caf83942853be3e58b79d67a1544ce482c0ef96
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-09 14:18:54 -0300

    update header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Header } from './componentes/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, Login, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
