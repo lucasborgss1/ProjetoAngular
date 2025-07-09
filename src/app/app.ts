c02fc1895c90a8c3e12adf12997e935c1a43e05b
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-08 21:11:03 -0300

    update app.component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './pages/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'ProjetoAngular';
